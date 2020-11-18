38e487a50a3545282d22163f78e27215ab86056a
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2020-11-18 10:33:14 +0700

    Java Thi

---
+++ StudentController.java
@@ -0,0 +1,37 @@
+package java2.Practice;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentController {
+    String url = "jdbc:mysql://localhost:3306/Student";
+    String username = "root";
+    String password = "";
+
+    public void displayStudent(List<Student> list){
+        System.out.printf("%-30s%-30s%-30s%-30s\n","StudentID","Student Name","Address","Phone");
+        System.out.printf("%-30s%-30s%-30s%-30s\n","B01010","Nguyen Tuan Anh ","Ha Noi","0904818238");
+        System.out.printf("%-30s%-30s%-30s%-30s\n","B10394","Nguyen Hoang Hai ","Hai Duong","0494949494");
+        for(int i = 0; i < list.size();i++){
+            System.out.print(list.get(i));
+        }
+    }
+    public boolean updateToDB(List<Student> list){
+        try (
+                Connection conn = DriverManager.getConnection(url, username, password);
+                Statement stmt = conn.createStatement();
+        ) {
+            String insert;
+            int count = 0;
+            for(int i = 0; i< list.size();i++){
+                insert = "insert into student(studentID,studentName,address,phone) values('"+list.get(i).getStudentID()+"','"+list.get(i).getName()+"','"+list.get(i).getAddress()+"','"+list.get(i).getPhone()+"'" +")";
+                count += stmt.executeUpdate(insert);
+            }
+            System.out.println(count + " row(s) affected");
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ StudentView.java
@@ -0,0 +1,26 @@
+package java2.Practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentView {
+    List<Student> stdList = new ArrayList<>();
+
+    public List<Student> addStudent(){
+        StudentController stdCrl = new StudentController();
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter Student ID: ");
+        String id = scanner.nextLine();
+        System.out.println("Enter Student Name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter Student Address:");
+        String address = scanner.nextLine();
+        System.out.println("Enter Student Phone:");
+        String phone = scanner.nextLine();
+        Student obj = new Student(id,name,address,phone);
+        stdList.add(obj);
+        System.out.println("Student has been added");
+        return stdList;
+    }
+}
